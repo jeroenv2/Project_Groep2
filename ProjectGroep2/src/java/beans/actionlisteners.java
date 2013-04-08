1a645c36fdec90215376e07cd2ae6f5379259da2
Steven Verheyen <destination.verheyen@gmail.com>
2013-04-08 15:06:21 +0200

    Merge-probleem opgelost

---
+++ ProjectGroep2/src/java/beans/actionlisteners.java
@@ -0,0 +1,27 @@
+package beans;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author robbie
+ */
+@ManagedBean
+@RequestScoped
+public class actionlisteners {
+
+    /**
+     * Creates a new instance of actionlisteners
+     */
+    public actionlisteners() {
+    }
+    public void dragstart() {
+        
+    }
+}
